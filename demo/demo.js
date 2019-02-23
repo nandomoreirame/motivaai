75eef550caf4e0538612a4666473a072b911d103
Felipe Pontes <felipemfpontes@gmail.com>
2019-02-23 11:51:52 -0300

    Add ability to display different backgrounds

--- demo/demo.js
+++ demo/demo.js
@@ -1,6 +1,6 @@
-function getPhrase () {
-  const phrases = MotivaAi.randonPhrase() // eslint-disable-line
-  let html = ``
+function getPhrase() {
+  const phrases = MotivaAi.randonPhrase(); // eslint-disable-line
+  let html = ``;
 
   phrases.forEach(phrase => {
     html += `
@@ -8,14 +8,31 @@ function getPhrase () {
         <p>${phrase.quote}</p>
         <small>${phrase.author}</small>
       </blockquote>
-      `
-  })
+      `;
+  });
 
-  return html
+  return html;
 }
 
-function meMotivaAi () {
-  document.getElementById('phrase').innerHTML = getPhrase()
+function randomColor() {
+  return "#000000".replace(/0/g, function() {
+    return (~~(Math.random() * 16)).toString(16);
+  });
 }
 
-meMotivaAi()
+function randomBackground() {
+  const $hero = document.querySelector(".hero");
+  $hero.style.backgroundImage =
+    "-webkit-linear-gradient(141deg, " +
+    randomColor() +
+    " , " +
+    randomColor() +
+    ")";
+}
+
+function meMotivaAi() {
+  document.getElementById("phrase").innerHTML = getPhrase();
+  randomBackground();
+}
+
+meMotivaAi();
