757cbf6d795eb4a14c78d69babceb06e8225fc6f
Fernando Moreira <nandomoreira.me@gmail.com>
2019-02-26 17:09:13 -0300

    lint demo.js file

--- demo/demo.js
+++ demo/demo.js
@@ -1,6 +1,6 @@
-function getPhrase() {
+function getPhrase () {
   const phrases = MotivaAi.randonPhrase(); // eslint-disable-line
-  let html = ``;
+  let html = ``
 
   phrases.forEach(phrase => {
     html += `
@@ -8,31 +8,31 @@ function getPhrase() {
         <p>${phrase.quote}</p>
         <small>${phrase.author}</small>
       </blockquote>
-      `;
-  });
+      `
+  })
 
-  return html;
+  return html
 }
 
-function randomColor() {
-  return "#000000".replace(/0/g, function() {
-    return (~~(Math.random() * 16)).toString(16);
-  });
+function randomColor () {
+  return '#000000'.replace(/0/g, function () {
+    return (~~(Math.random() * 16)).toString(16)
+  })
 }
 
-function randomBackground() {
-  const $hero = document.querySelector(".hero");
+function randomBackground () {
+  const $hero = document.querySelector('.hero')
   $hero.style.backgroundImage =
-    "-webkit-linear-gradient(141deg, " +
+    '-webkit-linear-gradient(141deg, ' +
     randomColor() +
-    " , " +
+    ' , ' +
     randomColor() +
-    ")";
+    ')'
 }
 
-function meMotivaAi() {
-  document.getElementById("phrase").innerHTML = getPhrase();
-  randomBackground();
+function meMotivaAi () {
+  document.getElementById('phrase').innerHTML = getPhrase()
+  randomBackground()
 }
 
-meMotivaAi();
+meMotivaAi()
